6910d7dd8eaeb3897919dc443b508de5d9a04f28
svegmond <svegmond@sogyo.nl>
2017-06-23 15:00:55 +0200

    Added Lee algorithm to folder

---
+++ AndroidApps/GridRobot/Lee Algorithm/src/lee/Direction.java
@@ -0,0 +1,5 @@
+package lee;
+
+public enum Direction {
+	NORTH, WEST, SOUTH, EAST
+}
---
+++ AndroidApps/GridRobot/Lee Algorithm/src/lee/Grid.java
@@ -0,0 +1,64 @@
+package lee;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+class Grid{
+	ArrayList<Vakje> content = new ArrayList<Vakje>();
+	int xDimension;
+	int yDimension;
+	
+	
+	public Grid(int xDimension, int yDimension, ArrayList<int[]> obstacles){
+		this.xDimension = xDimension;
+		this.yDimension = yDimension;
+		for (int y = 0 ; y <yDimension ; y++){
+			for (int x = 0 ; x < xDimension; x++){
+				if (isInList(obstacles,new int[]{x,y})){
+					content.add(new Vakje(x,y,-2));
+				}else{
+					content.add(new Vakje(x,y,-1));
+				}
+			}
+		}
+		
+	}
+	
+	private static boolean isInList(
+	        final List<int[]> list, final int[] candidate) {
+
+	    return list.stream().anyMatch(a -> Arrays.equals(a, candidate));
+	            //  ^-- or you may want to use .parallelStream() here instead
+	}
+	
+	public int getxDimension(){
+		return xDimension;
+	}
+	
+	public int getyDimension(){
+		return yDimension;
+	}
+	
+	public Vakje getVakje(int xcor,int ycor){
+		for (Vakje vakje : content){
+			if (vakje.getx() == xcor && vakje.gety() == ycor){
+				return vakje;
+			}
+		}
+		return null;
+	}
+	
+	public void print(){
+		int number = 0;
+		
+		for (Vakje vakje : content){
+			System.out.print(vakje.getValue());
+			number += 1;
+			if (number % xDimension==0){
+				System.out.println();
+			}
+		}
+	}
+}
